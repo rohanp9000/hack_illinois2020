a3e144b1044f53b679bcfeb3f546d5567276a52e
rohanp9000 <rohanp9000@gmail.com>
2020-02-29 14:21:18 -0600

    sriram retarded

--- lib/pages/ContactsPage.dart
+++ lib/pages/ContactsPage.dart
@@ -1,1 +1,58 @@
 import 'package:flutter/material.dart';
+
+class ContactsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          backgroundColor: Colors.black,
+          title: Text("Contacts",
+            style: TextStyle(
+                fontSize: 45,
+                fontFamily: 'Bebas',
+                fontWeight: FontWeight.bold
+            ),),
+
+        ),
+        body: new ContactList(kContacts));
+  }
+}
+
+const kContacts = const <Contact>[
+  const Contact(
+      fullName: 'kunal', phoneNumber: 'kuntboy@example.com'),
+  const Contact(
+      fullName: 'cheese', phoneNumber: 'bruh@example.com')
+];
+
+class ContactList extends StatelessWidget {
+  final List<Contact> _contacts;
+
+  ContactList(this._contacts);
+
+  @override
+  Widget build(BuildContext context) {
+    return new ListView.builder(
+      padding: new EdgeInsets.symmetric(vertical: 8.0),
+      itemBuilder: (context, index) {
+        return new _ContactListItem(_contacts[index]);
+      },
+      itemCount: _contacts.length,
+    );
+  }
+}
+
+class _ContactListItem extends ListTile {
+  _ContactListItem(Contact contact)
+      : super(
+      title: new Text(contact.fullName),
+      subtitle: new Text(contact.phoneNumber),
+      leading: new CircleAvatar(child: new Text(contact.fullName[0])));
+}
+
+class Contact {
+  final String fullName;
+  final String phoneNumber;
+
+  const Contact({this.fullName, this.phoneNumber});
+}
--- lib/pages/test.dart
+++ lib/pages/test.dart
@@ -133,25 +133,9 @@ class _DashboardState extends State<Test>
             ),
           ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Home'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            title: Text('Business'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            title: Text('School'),
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
+
     );
   }
+
+
 }
